534dbe4513c3cc701d1d6be5d32de2b7aa3e1b9e
mahmudnaeem <mahmudhasannaeem@gmail.com>
2023-05-09 15:48:33 +0600

    Json Parser project for Dorik

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-// import elements from "./data.json"
-// import Hello from "./alt-components/Hello";
-// import  RenderComponent  from "./functions/RenderComponent"
 import  RenderWeb  from "./functions/RenderWeb";
 
 function App() {
@@ -9,11 +6,6 @@ function App() {
   return (
     <div className="App">
       <RenderWeb />
-      {/* {elements.map((element)=>{
-        return (
-            <RenderComponent element={element} />
-        ) 
-      })}  */}
     </div>
   )
 }
